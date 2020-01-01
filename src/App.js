b60733d33e18a2aace775d8abb1e9a0745b0954f
bhavik103 <bhavikkalariya103@gmail.com>
2020-01-01 17:24:22 +0530

    added navigation to navigate with router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/hompage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
-  return <div><HomePage /></div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
